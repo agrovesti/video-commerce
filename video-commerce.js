ae70bc23acc7a2d6d3b1cd42bb4bfadee279903a
agrovesti <agrovesti2025@gmail.com>
2025-03-08 09:32:25 -0400

    video-commerce.js

---
+++ video-commerce.js
@@ -0,0 +1,6 @@
+var productLink = document.getElementById('product-link');
+window.onload = function() {
+  setTimeout(function() {
+    productLink.style.display = 'block';
+  }, 10000); // Exibe o link após 10 segundos
+};
